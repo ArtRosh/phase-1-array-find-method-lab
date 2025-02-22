235fbfa8b9df9ccdd0d8b1fa29ec8a2ae2f4f86b
ArtRosh <artem.rosh17@gmail.com>
2025-02-22 15:23:05 -0800

    lab-array

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record) {
+    const win = record.find(game => game.result === "W");
+    return win ? win.year : undefined;
+  }
+
+  const sadReality = [ { result: "N/A" }, { result: "N/A" }, { result: "N/A" } ];
+  superbowlWin(sadReality);  
